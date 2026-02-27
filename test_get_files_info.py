cfed7a5a968c7b77dea6feceba0d28e7f35a2ca3
Herzan <matteo.pati@gmail.com>
2026-02-27 21:13:30 +0100

    AI-agent access to directory content

---
+++ test_get_files_info.py
@@ -0,0 +1,20 @@
+import os
+from functions.get_files_info import get_files_info
+
+def test_get_files_info():
+    print("Result for current directory:")
+    print(f'  {get_files_info("calculator", ".")}')
+    print("Result for 'pkg' directory:")
+    print(f'  {get_files_info("calculator", "pkg")}')
+        
+    print("Result for '/bin' directory:")
+    print(f'    {get_files_info("calculator", "/bin")}')
+    
+    print("Result for '../' directory:")
+    print(f'    {get_files_info("calculator", "../")}')
+
+
+
+
+if __name__ == "__main__":
+    test_get_files_info()
